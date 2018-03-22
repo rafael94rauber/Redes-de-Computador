7d8ab15fe0caefc713ea489aa44dd4f14bc5a9ef
Rafael Eduardo Rauber <rafael94rauber@gmail.com>
2018-03-22 18:04:26 -0300

    via upload manual
    Subindo uma versao de teste da empresa, pode ser que esse breench esteja com erro e nao compile

---
+++ GravarMensagemTexto.cs
@@ -0,0 +1,47 @@
+﻿using GA.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace GA.WebAPI.Controllers.Actions
+{
+    public class GravarMensagemTexto
+    {
+        public const string nomeArquivo = @"D:\Bem_AnaliseDiaria\WEB_API.txt";
+        public void GravarMensagem(Mensagem mensagem)
+        {
+
+            StringBuilder conteudo = new StringBuilder();
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(nomeArquivo, append: true))
+            {
+                conteudo.AppendLine($"{Newtonsoft.Json.JsonConvert.SerializeObject(mensagem)}");
+
+                // escreve no arquivo a mensagem
+                file.Write(conteudo.ToString());
+            }
+        }
+
+        public Dictionary<int, string> PegarMensagem(int idMensagem, int idUsuarioReceber)
+        {
+            var conteudoTxt = string.Empty;
+            Dictionary<int, string> mansagemRetorno = null;
+
+            using (System.IO.StreamReader file = new System.IO.StreamReader(nomeArquivo))
+            {
+                mansagemRetorno = new Dictionary<int, string>();
+
+                while ((conteudoTxt = file.ReadLine()) != null)
+                {
+                    mansagemRetorno.Add(idMensagem, conteudoTxt);
+                }
+
+                file.Close();
+            }
+
+            return mansagemRetorno;
+        }
+    }
+}
---
+++ MensagemController.cs
@@ -0,0 +1,48 @@
+﻿using GA.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GA.WebAPI.Controllers
+{
+    public class MensagemController : ApiController
+    {
+        public Mensagem msg;
+
+
+        // GET: api/Mensagem
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Mensagem/5
+        public string Get(int idMensagem, int idUsuarioReceber)
+        {
+            Dictionary<int, string> teste = new Dictionary<int, string>();
+
+
+
+            Newtonsoft.Json.JsonConvert.DeserializeObject<Mensagem>(teste);
+
+            return "value";
+        }
+        
+        // POST: api/Mensagem
+        public void Post(int UsuarioEnvio, int UsuarioReceber, string conteudoMensagem)
+        {
+            Mensagem msg = new Mensagem
+            {
+                ConteudoMensagem = conteudoMensagem,
+                UsuarioEnviou = new Usuario().RetornarUsuario(UsuarioEnvio),
+                UsuarioRecebeu = new Usuario().RetornarUsuario(UsuarioReceber),
+                Id = new Random().Next(100)
+            };
+
+            //salvar a mensagem
+        }
+    }
+}
