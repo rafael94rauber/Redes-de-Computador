cdab714e6fadeeb8a14568b6f0f479d6eda5735c
Rafael Eduardo Rauber <rafael94rauber@gmail.com>
2018-03-26 19:08:15 -0300

    Add files via upload

--- GA.WebAPI/Controllers/MensagemController.cs
+++ GA.WebAPI/Controllers/MensagemController.cs
@@ -12,20 +12,20 @@ namespace GA.WebAPI.Controllers
         // GET: api/Mensagem
         [HttpGet]
         [ActionName("Receber")]
-        public IEnumerable<string> Get(int UsuarioEnvio, int UsuarioReceber)
+        public IHttpActionResult Get(int UsuarioEnvio, int UsuarioReceber)
         {
             GravarMensagemTexto adoMensgem = new GravarMensagemTexto();
-            var listaRetorno = adoMensgem.PegarMensagem(UsuarioEnvio, UsuarioReceber).ToList();
+            var listaRetorno = adoMensgem.PegarMensagem(UsuarioEnvio, UsuarioReceber);
 
             //issue Apagar mensagens recebidas #6 ************************
 
-            return listaRetorno;
+            return Ok(listaRetorno);
         }
 
         // POST: api/Mensagem/1/2/'mensagem de texto': exemplo de post
         [HttpPost]
         [ActionName("Enviar")]
-        public void Post(int UsuarioEnvio, int UsuarioReceber, string conteudoMensagem)
+        public IHttpActionResult Post(int UsuarioEnvio, int UsuarioReceber, string conteudoMensagem)
         {
             //Gerar ID para mensagens enviadas #5 ************************
             Mensagem msg = new Mensagem
@@ -39,6 +39,11 @@ namespace GA.WebAPI.Controllers
             //salvar a mensagem
             GravarMensagemTexto adoMensgem = new GravarMensagemTexto();
             adoMensgem.GravarMensagem(msg);
+
+            // retorna um objeto em forma de estrutura json
+            // estudar uma forma de retornar uma classe que seja util retornar
+            //issue Classe para retornar post das mensagens #7 ************************
+            return Ok(msg);
         }
     }
 }
