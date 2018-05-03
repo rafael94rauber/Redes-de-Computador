a4e82fef899554209e41bf49a4653e964d4ad27a
Rafael Eduardo Rauber <rafael94rauber@gmail.com>
2018-05-02 21:02:14 -0300

    upload manual
    para pode fazer em casa fazer sempre o upload manual e nao utilizar o git

---
+++ chatSocket/chatSocketClient/chatSocketClient/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace chatSocketClient
+{
+    class Program
+    {        
+        public static Thread TipoThread;
+
+        public static Dictionary<string, string> ListaClientes;
+
+        static void Main(string[] args)
+        {
+            ListaClientes = new Dictionary<string, string>();
+
+            string clienteNome = Console.ReadLine();
+
+            ListaClientes.Add(clienteNome, $"Cliente:{clienteNome}; id:{ListaClientes.Count()}");
+
+            ExecutarCliente(clienteNome);
+        }
+
+        private static void ExecutarCliente(string NomeCliente)
+        {
+            IPAddress localAddr = IPAddress.Parse("10.80.1.97");
+            TcpClient client = new TcpClient();
+            IPEndPoint serverLocation = new IPEndPoint(localAddr, 13000);
+
+            client.Connect(serverLocation);
+
+            //Se preferir altere localhost pelo IP do server
+
+            var sockStream = client.GetStream();
+
+            var escreve = new BinaryWriter(sockStream);
+
+            var le = new BinaryReader(sockStream);
+
+            var messageRespostaServidor = string.Empty;
+            StringBuilder todasMensagens = new StringBuilder();
+
+            do
+            {
+                Console.WriteLine("Mensagem Para enviar para o servidor: ");
+                var conteudoMensagem = Console.ReadLine();
+                var mensagemEnviarServidor = $"Cliente:{NomeCliente};ClienteReceber:{NomeCliente}-testeReceber;Mensagem:{conteudoMensagem}|";
+
+                Console.WriteLine($"mensagens enviados do cliente: {NomeCliente}");
+                Console.WriteLine($"mensagem: {conteudoMensagem}");
+
+                // manda para o servidor
+                escreve.Write(mensagemEnviarServidor);
+
+                // resposta do servidor
+                messageRespostaServidor = le.ReadString();
+
+                var ConteudoRespostaServidor = messageRespostaServidor.Split('|');
+
+                var ClienteReceber = ConteudoRespostaServidor[0].Split(';')[0];
+
+                string[] separadorMensagens = { "Mensagens:"};
+
+                string[] mensagens = ConteudoRespostaServidor[0].Split(separadorMensagens, StringSplitOptions.RemoveEmptyEntries);
+
+                Console.WriteLine($"Mensagens enviadas do cliente: {ClienteReceber}");
+                for (int i = 0; i < mensagens.Length; i++)
+                {
+                    Console.WriteLine($"Mensagem: {mensagens[i]}");
+                }
+
+                todasMensagens.AppendLine(messageRespostaServidor);
+
+            } while (messageRespostaServidor != "-1");
+
+            escreve.Close();
+
+            le.Close();
+
+            sockStream.Close();
+
+            client.Close();
+
+            TipoThread.Abort();
+        }
+    }
+}
---
+++ chatSocket/chatSocketServer/chatSocketServer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace chatSocketServer
+{
+    class Program
+    {
+        public static Thread TipoThread = null;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                TipoThread = new Thread(() => Servidor());
+
+                TipoThread.Start();                
+            }
+
+            while (TipoThread.IsAlive)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void Servidor()
+        {
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("10.80.1.97");
+
+            var server = new TcpListener(localAddr, port);
+
+            server.Start();
+
+            while (true)
+            {
+                var conexao = server.AcceptSocket();
+
+                var socketStream = new NetworkStream(conexao);
+
+                var escreve = new BinaryWriter(socketStream);
+                var le = new BinaryReader(socketStream);
+
+                var conteudo = "";
+
+                do
+                {
+                    conteudo = le.ReadString();
+
+                    var mensagemCliente = conteudo.Split('|');
+                    var conteudoClienteEnvio = mensagemCliente[0].Split(';')[0];
+                    var conteudoClienteReceber = mensagemCliente[0].Split(';')[1];
+                    var conteudoTexto = mensagemCliente[0].Split(';')[2];
+
+                    if (conteudoClienteReceber != conteudoClienteEnvio)
+                    {
+                        var respostaServidor = $"Cliente:{conteudoClienteReceber};Mensagens: mensagem 1 Mensagens:mensagem 2 Mensagens:mensagem 3 |";
+
+                        escreve.Write(conteudoTexto);
+                    }
+                    else
+                    {
+                        escreve.Write("-1");
+                    }                    
+                } while (conexao.Connected);
+
+                escreve.Close();
+                le.Close();
+                socketStream.Close();
+                conexao.Close();
+            }
+
+            server.Stop();
+        }
+    }
+}
